84ba6d269caf521b97d8470e80069a65a463126e
Jeremy R. Sellars <jeremyrsellars@gmail.com>
2020-04-27 17:26:41 -0500

    Remove code for pages

--- src/slatecljs/slate_walkthroughs.cljs
+++
@@ -1,8 +0,0 @@
-(ns slatecljs.slate-walkthroughs 
-  (:require [slatecljs.slate-01-installing-slate                      :as w01]
-            [slatecljs.slate-02-event-handlers                        :as w02]
-            [slatecljs.slate-03-defining-custom-elements              :as w03]))
-
-(def -main w03/-main)
-
-(-main)
