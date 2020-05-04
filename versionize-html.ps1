e79712baeae6f35e7e6bb7a2e665ac3cc21314c4
Jeremy R. Sellars <jeremyrsellars@gmail.com>
2020-05-04 11:19:09 -0500

    Use sha1 versions instead of date time

--- versionize-html.ps1
+++ versionize-html.ps1
@@ -1,11 +1,15 @@
 param (
   [string]$html,
-  [string]$version = (Get-Date -format "yyyyMMdd_HHmmss")
+  [string]$version
 )
+
+if ([string]::IsNullOrWhiteSpace($version)) {
+    $version = (get-filehash $html -algorithm sha1).Hash
+}
 
 Write-Output "Versionizing $($html) as $($version)"
 
 (Get-Content $html) `
-    -replace '(?<="[^:"]+\.(?:js|css|png|jpg))(?=")', "?Rel=$($version)" `
+    -replace '(?<="[^:"]+\.(?:js|css|png|jpg))(?=")', "?v=$($version)" `
     |
   Out-File $html -Force -Encoding ASCII
