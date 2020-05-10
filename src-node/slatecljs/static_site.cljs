472568160284ede1c42629c344418b9cccfd81fc
Jeremy R. Sellars <jeremyrsellars@gmail.com>
2020-05-10 17:41:28 -0500

    Extract/unify Introduction

--- src-node/slatecljs/static_site.cljs
+++ src-node/slatecljs/static_site.cljs
@@ -4,6 +4,7 @@
             [react :refer [createElement]]
             ["react-dom/server" :as ReactDOMServer]
             [slatecljs.common :as common]
+            slatecljs.slatecljs-introduction
             slatecljs.slate-01-installing-slate
             slatecljs.slate-02-event-handlers
             slatecljs.slate-03-defining-custom-elements
@@ -13,41 +14,6 @@
             [cljs-node-io.core :as io :refer [spit]]))
 
 (cljs.nodejs/enable-util-print!)
-
-(defn App
-  []
-  (createElement "div" #js {}
-    (createElement "a" #js {:href "https://docs.slatejs.org/"}
-      "Slate")
-    " is a DOM editor, but you get to define the Document Object Model " 
-    (createElement "em" #js {} "AND")
-    " the way that renders to HTML."
-    (createElement "p" #js {}
-      "These walkthroughs show how ClojureScript can be used to implement the JavaScript code from the official walkthroughs in the Slate documentation."
-      "ClojureScript offers a bunch of ways to make this easier, but I usually chose a very direct translation so they would be easier to follow along with the JS source."
-      "Let's save the power of ClojureScript for another time.")))
-
-(let [anchor ""
-      title "Introduction"]
-  (defn ^:export introduction
-    []
-    (slatecljs.common/render-demo
-      nil
-      {:title title
-       :anchor anchor
-       :about (App)
-       :objective "Feel free to read through the code or clone this repo and edit it to improve your understanding."
-       :navigation
-        (reduce into []
-         [(for [anchor ["w01" "w02" "w03" "w04" "w05" "w06"]]
-            {:text (common/title anchor)
-             :rendered-link (common/rendered-link anchor)})
-          [{:text "01 Installing Slate"
-            :url "https://docs.slatejs.org/walkthroughs/01-installing-slate"
-            :class "slate-tutorial"}]])}))
-
-  (defmethod common/app-component anchor [_] introduction)
-  (defmethod common/title anchor [_] title))
 
 (defn shell
   [{:keys [title anchor]} app-content]
